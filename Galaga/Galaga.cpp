2c667bdd3daaa7f163ba656b84e92fb9af300cc0
Ankeey <79295253+AnkeetPrasai@users.noreply.github.com>
2021-03-25 18:04:09 -0500

    I think i got it to work

--- Galaga/Galaga.cpp
+++ Galaga/Galaga.cpp
@@ -1,20 +1,30 @@
-// Galaga.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "SFML\System.hpp"
+#include "SFML\Graphics.hpp"
+#include "SFML\Window.hpp"
+#include "SFML\Audio.hpp"
+#include "SFML\Network.hpp"
+
+using namespace sf;
 
 int main()
 {
+    RenderWindow window(VideoMode(1280, 720), "GALAGA", Style::Default);
+
+
+    while (window.isOpen()) {
+        Event e;
+        while (window.pollEvent(e)) {
+            if (e.type == Event::Closed)
+                window.close();
+
+        }
+
+        window.clear();
+
+        window.display();
+    }
     std::cout << "Hello World!\n";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
