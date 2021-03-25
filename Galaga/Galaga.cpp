7d222840673fdde1b7a7ed0feeeb5b928f73d8bc
Ankeey <79295253+AnkeetPrasai@users.noreply.github.com>
2021-03-25 18:20:25 -0500

    shape test
    :)

--- Galaga/Galaga.cpp
+++ Galaga/Galaga.cpp
@@ -9,19 +9,30 @@ using namespace sf;
 
 int main()
 {
-    RenderWindow window(VideoMode(1280, 720), "GALAGA", Style::Default);
-
+    RenderWindow window(VideoMode(1280, 720), "GALAGA", Style::Default); //style chooses between windowed and fullscreen
+                        //Videomode you can set size
+    window.setFramerateLimit(30);
+                        //Rate at which window updates
 
     while (window.isOpen()) {
         Event e;
+
+
+        CircleShape shape(220.f);
+
+                    // Checks for any input from user and it send it to event handler
         while (window.pollEvent(e)) {
             if (e.type == Event::Closed)
                 window.close();
 
         }
 
+        //Clears the window
         window.clear();
 
+        window.draw(shape);
+
+        //Displays the new frame
         window.display();
     }
     std::cout << "Hello World!\n";
