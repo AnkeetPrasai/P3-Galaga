d4d24d49ff543932a2dcb1e135153b4c75d54f66
Alex Wilhelm <alexwilhelm@ku.edu>
2021-04-02 13:03:16 -0500

    Game Restarts after win & lose

--- Galaga/Galaga.cpp
+++ Galaga/Galaga.cpp
@@ -111,7 +111,7 @@ void EndScreen()
           {
             if (e.key.code == Keyboard::Space)
             {
-              display.window.close();
+                  return;
             }
           }
       }
@@ -152,190 +152,199 @@ void WinScreen()
           {
             if (e.key.code == Keyboard::Space)
             {
-              display.window.close();
+              return;
             }
           }
       }
   }
 }
 
+void Play()
+{
+  int test = 0;
+  do
+  {
+    display.window.clear();
+    int check1 = 1;
+    int check = 1;
+    Text score;
+    Font font;
+    //if (!font.loadFromFile())
+   //  {
+   // //     return -1;
+   //  }
+
+    sf::Music music;
+    if (!music.openFromFile("GalagaTheme.ogg"))
+    {
+        return; // error
+    }
+    music.play();
+    StartScreen();
+    music.stop();
+
+      Player p;
+      vector<Enemy> enemies;
+      for (int i = 0; i < 20; i++)
+      {
+          enemies.push_back(Enemy(i * 30 + 300, 0));
+      }
+
+      int time = 0;
+      bool left = false;
+      string scoreDisplay;
+      score.setStyle(Text::Bold);
+      score.setFont(font);
+      while (display.window.isOpen() && check1 == 1)
+      {
+          scoreDisplay = to_string(p.getScore());
+          score.setString(scoreDisplay);
+          Event e;
+          // Checks for any input from user and it send it to event handler
+          while (display.window.pollEvent(e)||check && check1 == 1)
+          {
+              if (e.type == Event::KeyPressed)
+              {
+                  if (e.key.code == Keyboard::Space)
+                  {
+                    // if (e.key.code == Keyboard::Space && e.key.code == Keyboard::A)
+                    // {
+                    // p.Movement(e);
+                    p.Shoot();
+                    if (!music.openFromFile("Laser.ogg"))
+                    {
+                        return; // error
+                    }
+                    music.play();
+                  }
+                  else if (e.key.code == Keyboard::Escape)
+                  {
+                      display.window.close();
+                      check = 0;
+                  }
+                  else
+                  {
+                      p.Movement(e);
+                  }
+              // }
+            }
+              if (e.type == Event::Closed)
+              {
+                  display.window.close();
+                  check = 0;
+              }
+              if (p.gameOver())
+              {
+                check1 = 0;
+                EndScreen();
+              }
+              if (enemies.size() == 0)
+              {
+                check1 = 0;
+                check = 0;
+                WinScreen();
+              }
+
+              int selection = 0;
+              int random = 0;
+              if (enemies.size() == 0) // Breaks out of the loop if the player wins
+              {
+                  break;
+              }
+              int check = 0;
+              int previous = 0;
+              selection = rand() % enemies.size(); //Randomly selects an enemy ship
+              random = rand() % enemies.size();
+              for (int i = 0; i < enemies.size(); i++) //Checks if any ship has started moving
+              {
+                  if (enemies[i].getShape().getPosition().y == 0)
+                  {
+                      check++;
+                  }
+                  else
+                  {
+                      previous = i;
+                  }
+              }
+              if (check == enemies.size()) //If no enemy has moved, the randomly selected one moves
+              {
+                  enemies[selection].move();
+                  enemies[random].Shoot();
+              }
+              else //Otherwise the one that has moved continues moving
+              {
+                  enemies[previous].move();
+              }
+              if (time == 0)
+              {
+                  left = true;
+              }
+              else
+              {
+                  time--;
+              }
+              for (int i = 0; i < enemies.size(); i++)
+              {
+                  enemies[i].backAndForth(left);
+              }
+              if (enemies[0].getShape().getPosition().x < 100 && time == 0)
+              {
+                  time = 200;
+                  left = false;
+                  enemies[random].Shoot();
+              }
+              //Clears the window
+              display.window.clear();
+
+              for (int i = 0; i < p.getProjectiles().size(); i++) //Updates the projectiles with their new positions
+              {
+                  p.updateProjectiles(i, enemies);
+              }
+
+              for (int i = 0; i < enemies.size(); i++)
+              {
+                  for (int j = 0; j < enemies[i].getProjectiles().size(); j++)
+                  {
+                      enemies[i].updateProjectiles(j);
+                  }
+              }
+
+              display.window.draw(p.getShape()); //Draws the player on the screen
+
+              for (int i = 0; i < p.getProjectiles().size(); i++) //Draws the player's projectiles on the screen
+              {
+                  display.window.draw(p.getProjectiles()[i]);
+              }
+
+              for (int i = 0; i < enemies.size(); i++) //Draws the enemies on the screen
+              {
+                  display.window.draw(enemies[i].getShape());
+              }
+
+              for (int i = 0; i < enemies.size(); i++)
+              {
+                  for (int j = 0; j < enemies[i].getProjectiles().size(); j++)
+                  {
+                      display.window.draw(enemies[i].getProjectiles()[j]);
+                  }
+              }
+
+              display.window.draw(score);
+
+              //Displays the new frame
+              display.window.display();
+              for (int i = 0; i < enemies.size(); i++)
+              {
+                  p.checkCollison(enemies[i]);
+              }
+          }
+        }
+    } while (test == 0);
+}
+
 int main()
 {
-  int check = 1;
-  Text score;
-  Font font;
-  //if (!font.loadFromFile())
-  {
- //     return -1;
-  }
-
-  sf::Music music;
-  if (!music.openFromFile("GalagaTheme.ogg"))
-  {
-      return -1; // error
-  }
-  music.play();
-  StartScreen();
-  music.stop();
-
-    Player p;
-    vector<Enemy> enemies;
-    for (int i = 0; i < 20; i++)
-    {
-        enemies.push_back(Enemy(i * 30 + 300, 0));
-    }
-
-    int time = 0;
-    bool left = false;
-    string scoreDisplay;
-    score.setStyle(Text::Bold);
-    score.setFont(font);
-    while (display.window.isOpen())
-    {
-        scoreDisplay = to_string(p.getScore());
-        score.setString(scoreDisplay);
-        Event e;
-        // Checks for any input from user and it send it to event handler
-        while (display.window.pollEvent(e)||check)
-        {
-            if (e.type == Event::KeyPressed)
-            {
-                if (e.key.code == Keyboard::Space)
-                {
-                  // if (e.key.code == Keyboard::Space && e.key.code == Keyboard::A)
-                  // {
-                  // p.Movement(e);
-                  p.Shoot();
-                  if (!music.openFromFile("Laser.ogg"))
-                  {
-                      return -1; // error
-                  }
-                  music.play();
-                }
-                else if (e.key.code == Keyboard::Escape)
-                {
-                    display.window.close();
-                    check = 0;
-                }
-                else
-                {
-                    p.Movement(e);
-                }
-            // }
-          }
-            if (e.type == Event::Closed)
-            {
-                display.window.close();
-                check = 0;
-            }
-            if (p.gameOver())
-            {
-              check = 0;
-              EndScreen();
-            }
-            if (enemies.size() == 0)
-            {
-              check = 0;
-              WinScreen();
-            }
-
-            int selection = 0;
-            int random = 0;
-            if (enemies.size() == 0) // Breaks out of the loop if the player wins
-            {
-                break;
-            }
-            int check = 0;
-            int previous = 0;
-            selection = rand() % enemies.size(); //Randomly selects an enemy ship
-            random = rand() % enemies.size();
-            for (int i = 0; i < enemies.size(); i++) //Checks if any ship has started moving
-            {
-                if (enemies[i].getShape().getPosition().y == 0)
-                {
-                    check++;
-                }
-                else
-                {
-                    previous = i;
-                }
-            }
-            if (check == enemies.size()) //If no enemy has moved, the randomly selected one moves
-            {
-                enemies[selection].move();
-                enemies[random].Shoot();
-            }
-            else //Otherwise the one that has moved continues moving
-            {
-                enemies[previous].move();
-            }
-            if (time == 0)
-            {
-                left = true;
-            }
-            else
-            {
-                time--;
-            }
-            for (int i = 0; i < enemies.size(); i++)
-            {
-                enemies[i].backAndForth(left);
-            }
-            if (enemies[0].getShape().getPosition().x < 100 && time == 0)
-            {
-                time = 200;
-                left = false;
-                enemies[random].Shoot();
-            }
-            //Clears the window
-            display.window.clear();
-
-            for (int i = 0; i < p.getProjectiles().size(); i++) //Updates the projectiles with their new positions
-            {
-                p.updateProjectiles(i, enemies);
-            }
-
-            for (int i = 0; i < enemies.size(); i++)
-            {
-                for (int j = 0; j < enemies[i].getProjectiles().size(); j++)
-                {
-                    enemies[i].updateProjectiles(j);
-                }
-            }
-
-            display.window.draw(p.getShape()); //Draws the player on the screen
-
-            for (int i = 0; i < p.getProjectiles().size(); i++) //Draws the player's projectiles on the screen
-            {
-                display.window.draw(p.getProjectiles()[i]);
-            }
-
-            for (int i = 0; i < enemies.size(); i++) //Draws the enemies on the screen
-            {
-                display.window.draw(enemies[i].getShape());
-            }
-
-            for (int i = 0; i < enemies.size(); i++)
-            {
-                for (int j = 0; j < enemies[i].getProjectiles().size(); j++)
-                {
-                    display.window.draw(enemies[i].getProjectiles()[j]);
-                }
-            }
-
-            display.window.draw(score);
-
-            //Displays the new frame
-            display.window.display();
-            for (int i = 0; i < enemies.size(); i++)
-            {
-                p.checkCollison(enemies[i]);
-            }
-        }
-
-
-        std::cout << "Hello World!\n";
-
-        return 0;
-    }
+  Play();
+  std::cout << "Hello World!\n";
+  return 0;
 }
